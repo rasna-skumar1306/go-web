f782e9a4a88bacdafcd37468639229e47cbb660f
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-17 18:40:50 +0530

    program to pass a value into a template

--- Example1/main/main.go
+++ Example1/main/main.go
@@ -1,17 +1,18 @@
+//program to pass a value into a template
 package main
 
 import (
 	"log"
 	"os"
-	"text/template"
+	"text/template" //template package for parsing files
 )
 
 func main() {
-	tpl, err := template.ParseFiles("index.gohtml")
+	tpl, err := template.ParseFiles("index.gohtml") //parse a gohtml file and getting tpl of type *template.Template
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.Execute(os.Stdout, "Rasswanth")
+	err = tpl.Execute(os.Stdout, "Rasswanth") //passing data into template
 	if err != nil {
 		log.Fatalln(err)
 	}
