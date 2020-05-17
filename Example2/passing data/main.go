c27e42c274d80185a1e15ce7d0ed5775338c9228
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-17 18:42:01 +0530

    example 2 for passing value into template

--- Example2/passing data/main.go
+++ Example2/passing data/main.go
@@ -1,3 +1,4 @@
+//example 2 for passing value into template
 package main
 
 import (
@@ -6,14 +7,14 @@ import (
 	"text/template"
 )
 
-var tpl *template.Template
+var tpl *template.Template //declaring a *template.Template for parsing file
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.gohtml"))
+	tpl = template.Must(template.ParseFiles("index.gohtml")) //using template.Must for handling err and getting tpl *template.Template
 }
 
 func main() {
-	err := tpl.Execute(os.Stdout, `Rasswanth Senthilkumar S/o Senthilkumar`)
+	err := tpl.Execute(os.Stdout, `Rasswanth Senthilkumar S/o Senthilkumar`) //passing a raw string literal as value
 	if err != nil {
 		log.Fatalln(err)
 	}
